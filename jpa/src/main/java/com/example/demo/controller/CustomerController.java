ac8bc54934211d4f95804dc0eb8b3efbd889c3a4
xing <xinghua24@gmail.com>
2019-07-18 17:43:16 -0400

    add Controller

--- jpa/src/main/java/com/example/demo/controller/CustomerController.java
+++ jpa/src/main/java/com/example/demo/controller/CustomerController.java
@@ -11,7 +11,7 @@ import com.example.demo.entity.Customer;
 import com.example.demo.service.CustomerService;
 
 @RestController
-@RequestMapping(value="/customers")
+@RequestMapping(value = "/customers")
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
@@ -19,27 +19,27 @@ public class CustomerController {
     // localhost:8080/customers
     @RequestMapping("")
     public List<Customer> getCustomers() {
-        return customerService.findAll();
+	return customerService.findAll();
     }
 
     // localhost:8080/customers?email=alice@example.com
     @RequestMapping(value = "/", params = "email")
     public List<Customer> getCustomerByEmail(@RequestParam("email") String email) {
-        return customerService.findByEmail( email );
+	return customerService.findByEmail(email);
     }
 
     // http://localhost:8080/customers/byregdate?start=2019-01-01&end=2019-01-31
-    @RequestMapping(value = "/byregdate", params = {"start", "end"})
+    @RequestMapping(value = "/byregdate", params = { "start", "end" })
     public List<Customer> getCustomerByEmail(
-            @RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start, 
-            @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
-        return customerService.findByRegDateRange(start, end);
+	    @RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
+	    @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
+	return customerService.findByRegDateRange(start, end);
     }
-    
+
     // localhost:8080/customers/testTransaction
     @RequestMapping("/testTransaction")
     public String testTransaction() {
-        customerService.deleteCustomer(2L);
-        return "delete completed";
+	customerService.deleteCustomer(2L);
+	return "delete completed";
     }
 }
