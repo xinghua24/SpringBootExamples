e355a3464c247c7a0840627e68d2a2e6840c84dd
Xinghua Zhang <xinghua24@gmail.com>
2022-10-06 11:43:20 -0400

    update actuator project

--- actuator/src/main/java/com/example/demo/Application.java
+++ actuator/src/main/java/com/example/demo/Application.java
@@ -1,14 +1,23 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Scheduled(fixedRate = 10000L)
+	public void someJob() throws InterruptedException {
+		System.out.println("Current Time is " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+	}
 }
 
